2d04282a00962ef626f63a445f51636228d1ee2b
dhanush2108 <113632642+dhanush2108@users.noreply.github.com>
2022-11-23 14:13:53 +0530

    Add files via upload

--- Project Development  Phase/Sprint-4/flask/app.py
+++ Project Development  Phase/Sprint-4/flask/app.py
@@ -1,163 +1,282 @@
-from flask import Flask,render_template,Response
-import pyttsx3
-import os
-import cv2
-import numpy as np
-import time
-import mediapipe as mp
-from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
 
-app=Flask(__name__)
+from flask import Flask, render_template, flash, request, session
+from flask import render_template, redirect, url_for, request
 
-mp_holistic = mp.solutions.holistic # Holistic model
-mp_drawing = mp.solutions.drawing_utils # Drawing utilities
-mp_pose = mp.solutions.pose
 
-def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False                  # Image is no longer writeable
-    results = model.process(image)                 # Make prediction
-    image.flags.writeable = True                   # Image is now writeable 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
-    return image, results
 
-def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw face connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
+import smtplib
 
-def draw_styled_landmarks(image, results):
-    # Draw face connections
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
-                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                             ) 
-    # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                             ) 
-    # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                             ) 
-    # Draw right hand connections  
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                             ) 
+from cloudant.client import  Cloudant
 
-def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([pose, face, lh, rh])
 
-actions = np.array(['exmonsoon','fall','season','spring','summer','winter'])
 
-# 1. New detection variables
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import TensorBoard
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(60,1662)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
-model.add(LSTM(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(actions.shape[0], activation='softmax'))
+client = Cloudant.iam("2cbf492c-e69a-4763-8424-f608b7a3259e-bluemix","uDu-_l8TtYZbHHtBwW2Q0NvX_nhEhVAQ0pZdHfpYGpHp",connect=True)
+my_database = client.create_database("database")
 
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.load_weights('model.h5')
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-    
-sequence = []
-sentence = []
-predictions = []
-threshold = 0.5
-cap=cv2.VideoCapture(0)
-def generate_frames():
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        global sequence
-        global sentence
-        global predictions
-        global threshold
-        while True:
+app.config['DEBUG']
 
-        # Read feed
-            success, frame = cap.read()
-            if not success:
-                break
+
+@app.route("/")
+def homepage():
+    return render_template('index.html')
+
+
+@app.route("/VoicetoText")
+def VoicetoText():
+    return render_template('index1.html')
+
+@app.route("/UserLogin")
+def UserLogin():
+    return render_template('UserLogin.html')
+
+
+
+@app.route("/Newuser")
+def Newuser():
+    return render_template('Newuser.html')
+
+
+
+@app.route("/newuse",methods=['GET','POST'])
+def newuse():
+    if request.method == 'POST':#
+
+        x = [x for x in request.form.values()]
+        print(x)
+        data = {
+            '_id': x[1],
+            'name': x[0],
+            'psw': x[2]
+        }
+        print(data)
+        query = {'_id': {'Seq': data['_id']}}
+        docs = my_database.get_query_result(query)
+        print(docs)
+        print(len(docs.all()))
+        if (len(docs.all()) == 0):
+            url = my_database.create_document(data)
+            return render_template('goback.html', data="Register, please login using your details")
+        else:
+            return render_template('goback.html', data="You are already a member, please login using your details")
+
+@app.route("/userlog", methods=['GET', 'POST'])
+def userlog():
+        if request.method == 'POST':
+
+            user = request.form['_id']
+            passw = request.form['psw']
+            print(user, passw)
+
+            query = {'_id': {'$eq': user}}
+            docs = my_database.get_query_result(query)
+            print(docs)
+            print(len(docs.all()))
+            if (len(docs.all()) == 0):
+                return render_template('goback.html', pred="The username is not found.")
             else:
-            # Make detections
-                image, results = mediapipe_detection(frame, holistic)
-                print(results)
-                
-                # Draw landmarks
-                draw_styled_landmarks(image, results)
-                
-                # 2. Prediction logic
-                keypoints = extract_keypoints(results)
-                sequence.append(keypoints)
-                sequence = sequence[-60:]
-                
-                if len(sequence) == 60:
-                    res = model.predict(np.expand_dims(sequence, axis=0))[0]
-                    print(actions[np.argmax(res)])
-                    predictions.append(np.argmax(res))
-                    
-                    
-                #3. Viz logic
-                    if np.unique(predictions[-10:])[0]==np.argmax(res): 
-                        if res[np.argmax(res)] > threshold: 
-                            
-                            if len(sentence) > 0: 
-                                if actions[np.argmax(res)] != sentence[-1]:
-                                    sentence.append(actions[np.argmax(res)])
-                            else:
-                                sentence.append(actions[np.argmax(res)])
+                if ((user == docs[0][0]['_id'] and passw == docs[0][0]['psw'])):
 
-                    if len(sentence) > 5: 
-                        sentence = sentence[-5:]
+                    return render_template("userhome.html")
+                else:
+                    return render_template('goback.html',data="user name and password incorrect")
 
-                    # Viz probabilities
-                    # image = prob_viz(res, actions, image, colors)
-                    
-                    cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
-                    engine = pyttsx3.init()
-                    engine.say(sentence)
-                    engine.runAndWait()
-                    cv2.putText(image, ' '.join(sentence), (3,30), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                
-                # Show to screen
-                # cv2.imshow('OpenCV Feed', image)
 
-                if cv2.waitKey(10) & 0xFF == ord('q'):
+
+@app.route("/start", methods=['GET', 'POST'])
+def start():
+    error = None
+    if request.method == 'POST':
+        if request.form["submit"] == "AlphabetDetection":
+            import csv
+            import copy
+            import cv2 as cv
+            import mediapipe as mp
+            from model import KeyPointClassifier
+            from app_files import calc_landmark_list, draw_info_text, draw_landmarks, get_args, pre_process_landmark
+            from PIL import Image, ImageDraw, ImageFont
+            import numpy as np
+
+            args = get_args()
+            cap_device = args.device
+            cap_width = args.width
+            cap_height = args.height
+
+            use_static_image_mode = args.use_static_image_mode
+            min_detection_confidence = args.min_detection_confidence
+            min_tracking_confidence = args.min_tracking_confidence
+
+            cap = cv.VideoCapture(cap_device)
+            cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
+            cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
+
+            mp_hands = mp.solutions.hands
+            hands = mp_hands.Hands(
+                static_image_mode=use_static_image_mode,
+                max_num_hands=1,
+                min_detection_confidence=min_detection_confidence,
+                min_tracking_confidence=min_tracking_confidence,
+            )
+
+            keypoint_classifier = KeyPointClassifier()
+
+            with open('model/keypoint_classifier/keypoint_classifier_label.csv', encoding='utf-8-sig') as f:
+                keypoint_classifier_labels = csv.reader(f)
+                keypoint_classifier_labels = [
+                    row[0] for row in keypoint_classifier_labels
+                ]
+
+            flag = 0
+
+            import win32com.client as wincl
+            speak = wincl.Dispatch("SAPI.SpVoice")
+
+            while True:
+                key = cv.waitKey(10)
+                if key == 27:  # ESC
                     break
-                # cap.release()
-                # cv2.destroyAllWindows()
-                ret,buffer=cv2.imencode('.jpg',image)
-                s=buffer.tobytes()
-                yield(b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + s + b'\r\n')
 
-@app.route('/')
-def index():
-    return render_template('home.html')
-@app.route('/upload')
+                ret, image = cap.read()
+                if not ret:
+                    break
+                image = cv.flip(image, 1)
+                debug_image = copy.deepcopy(image)
+                # print(debug_image.shape)
+                # cv.imshow("debug_image",debug_image)
+                image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-def upload():
-    return render_template('upload.html')
+                image.flags.writeable = False
+                results = hands.process(image)
+                image.flags.writeable = True
 
-@app.route('/video')
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+                if results.multi_hand_landmarks is not None:
+                    for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+                        landmark_list = calc_landmark_list(debug_image, hand_landmarks)
 
-if __name__=="__main__":
-    app.run(debug=True)
+                        # print(hand_landmarks)
+                        pre_processed_landmark_list = pre_process_landmark(landmark_list)
+
+                        hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+
+                        debug_image = draw_landmarks(debug_image, landmark_list)
+                        flag += 1
+                        print(flag)
+                        if (flag == 100):
+                            flag = 0
+
+                            speak.Speak(keypoint_classifier_labels[hand_sign_id])
+
+                        debug_image = draw_info_text(
+                            debug_image,
+                            handedness,
+                            keypoint_classifier_labels[hand_sign_id])
+
+                cv.imshow('Hand Gesture Recognition', debug_image)
+
+            cap.release()
+            cv.destroyAllWindows()
+        else:
+
+            import cv2
+            import numpy as np
+            import mediapipe as mp
+            import tensorflow as tf
+            from tensorflow.keras.models import load_model
+
+            # initialize mediapipe
+            mpHands = mp.solutions.hands
+            hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+            mpDraw = mp.solutions.drawing_utils
+
+            # Load the gesture recognizer model
+            model = load_model('mp_hand_gesture')
+
+            # Load class names
+            f = open('gesture.names', 'r')
+            classNames = f.read().split('\n')
+            f.close()
+            print(classNames)
+
+            # Initialize the webcam
+            cap = cv2.VideoCapture(0)
+
+            while True:
+                # Read each frame from the webcam
+                _, frame = cap.read()
+
+                x, y, c = frame.shape
+
+                # Flip the frame vertically
+                frame = cv2.flip(frame, 1)
+                framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                # Get hand landmark prediction
+                result = hands.process(framergb)
+
+                # print(result)
+
+                className = ''
+
+                # post process the result
+                if result.multi_hand_landmarks:
+                    landmarks = []
+                    for handslms in result.multi_hand_landmarks:
+                        for lm in handslms.landmark:
+                            # print(id, lm)
+                            lmx = int(lm.x * x)
+                            lmy = int(lm.y * y)
+
+                            landmarks.append([lmx, lmy])
+
+                        # Drawing landmarks on frames
+                        mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+
+                        # Predict gesture
+                        prediction = model.predict([landmarks])
+                        # print(prediction)
+                        classID = np.argmax(prediction)
+                        className = classNames[classID]
+
+                # show the prediction on the frame
+                cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                            1, (0, 0, 255), 2, cv2.LINE_AA)
+
+                # Show the final output
+                cv2.imshow("Output", frame)
+
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+            # release the webcam and destroy all active windows
+            cap.release()
+
+            cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+    return render_template('UserHome.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
